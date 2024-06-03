0e98e083b24f5ed226edcd53e82c1b7b197e4521
TalesPalma <tales.palma_2022@outlook.com>
2024-06-03 16:46:49 -0300

    First versions

---
+++ src/app/component/container/container.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+
+@Component({
+  selector: 'app-container',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './container.component.html',
+  styleUrl: './container.component.css'
+})
+export class ContainerComponent {
+
+
+
+
+
+}
