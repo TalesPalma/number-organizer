cebe91d8c4cb4ec88bf39608e5e1e4912973b8f6
Tales Palma <tales.palma_2022@outlook.com>
2024-06-10 15:11:33 -0300

    Form validations and buttons validations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ContainerComponent } from './component/container/container.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "Container",
+    component: ContainerComponent
+  },
+  {
+    path: '',
+    redirectTo: '/Container',
+    pathMatch: 'full'
+
+  }
+];
